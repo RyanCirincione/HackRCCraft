eb55571f655d5f84093868a6e3fca968dcccf69b
Ryguy9999 <ryancirincione@aol.com>
2016-10-22 15:37:21 -0400

    More Java2D base: input

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,10 @@
+import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -31,6 +37,48 @@ public class Game extends JPanel
 	}
 	
 	static int G_WIDTH = 800, G_HEIGHT = 600;
+	
+	public Game()
+	{
+		this.setPreferredSize(new Dimension(G_WIDTH, G_HEIGHT));
+		
+		this.addMouseMotionListener(new MouseMotionAdapter(){
+			public void mouseMoved(MouseEvent e)
+			{
+				
+			}
+			
+			public void mouseDragged(MouseEvent e)
+			{
+				//MOUSEMOVED WONT BE CALLED IF A BUTTON IS DOWN DURING MOVEMENT
+			}
+		});
+		this.addMouseListener(new MouseAdapter(){
+			public void mousePressed(MouseEvent e)
+			{
+				
+			}
+			
+			public void mouseReleased(MouseEvent e)
+			{
+				
+			}
+		});
+		this.addKeyListener(new KeyAdapter(){
+			public void keyPressed(KeyEvent e)
+			{
+				
+			}
+			
+			public void keyReleased(KeyEvent e)
+			{
+				
+			}
+		});
+		
+		this.setFocusable(true);
+		this.requestFocus();
+	}
 	
 	public void tick()
 	{
