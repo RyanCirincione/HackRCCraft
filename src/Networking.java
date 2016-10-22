cd4587034816b4ef0cbcfd7512c3da53bd5e70f7
Ryan Goldstein <ryanisaacg@gmail.com>
2016-10-22 17:34:11 -0400

    I think the network layer is less than 100 lines?

---
+++ src/Networking.java
@@ -0,0 +1,67 @@
+import java.io.IOException;
+
+import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.EndPoint;
+import com.esotericsoftware.kryonet.Listener;
+import com.esotericsoftware.kryonet.Server;
+import com.esotericsoftware.kryonet.rmi.ObjectSpace;
+
+public class Networking {
+	class NetworkedInstance extends Listener {
+		EndPoint point;
+		State state;
+		Connection other;
+		
+		NetworkedInstance() {
+			state = new State(2);
+		}
+		
+		public void start() {
+			point.getKryo().register(State.class);
+			point.addListener(this);
+		}
+		
+		public void received (Connection connection, Object object) {
+			if(other == null) {
+				ObjectSpace.registerClasses(point.getKryo());
+				ObjectSpace objectSpace = new ObjectSpace();
+				objectSpace.register(0, state);
+				objectSpace.addConnection(connection);
+			}
+			other = connection;
+	   }
+		
+		void update() {
+			state.update();
+			State otherState = ObjectSpace.getRemoteObject(other, 0, State.class);
+			otherState.merge(state);
+		}
+	}
+	
+	class GameServer extends NetworkedInstance {
+		
+		GameServer() throws IOException {
+			super();
+			Server server = new Server();
+			server.start();
+			server.bind(8888, 8889);
+			point = server;
+			start();
+			
+		}
+	}
+	class GameClient extends NetworkedInstance {
+		
+		GameClient(String ip) throws IOException {
+			super();
+			Client client = new Client();
+			client.start();
+			client.connect(5000, ip, 8888, 8889);
+			point = client;
+			start();
+		}
+		
+		
+	}
+}
