5bc40676fe514f8229050828dc8f0bb34728ae11
Ryan Goldstein <ryanisaacg@gmail.com>
2016-10-23 00:16:47 -0400

    Added some initial bounds values to the barracks

--- src/game/buildings/Barracks.java
+++ src/game/buildings/Barracks.java
@@ -1,7 +1,9 @@
 package game.buildings;
 
 import game.Building;
+import game.Circle;
 import game.State;
+import game.Unit;
 import game.units.SimpleRobot;
 
 public class Barracks extends Building
@@ -14,7 +16,13 @@ public class Barracks extends Building
 		if(cooldown == 0)
 		{
 			State.Shard newShard = state.shards[this.shard];
-			newShard.units.get(this.player).add(new SimpleRobot());
+			Unit robot = new SimpleRobot();
+			Circle circ = new Circle();
+			circ.setRadius(32);
+			robot.box = circ;
+			robot.box.setX(box.x());
+			robot.box.setY(box.y());
+			newShard.units.get(this.player).add(robot);
 			cooldown = 60;
 		}
 		else
